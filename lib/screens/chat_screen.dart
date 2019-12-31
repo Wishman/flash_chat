366679ac3cc36d758e4459f7cbb02c9bfc902020
Wishman <gjwisher@aol.com>
2019-12-31 17:50:40 +0100

    14. L-178: The Flutter ListView Task 1: improve styling of Chat app by adding ListView et. al. Task 2: refactor: extract StreamBuilder, clear text field when send is hit

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -3,6 +3,8 @@ import 'package:flash_chat/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart'; // 8.7(a)
 import 'package:cloud_firestore/cloud_firestore.dart'; // 11.1
 
+final _firestore = Firestore.instance; // 11.4 & 14.16(d)
+
 class ChatScreen extends StatefulWidget {
   // 1.1
   static String id = 'chat_screen';
@@ -12,7 +14,7 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-  final _firestore = Firestore.instance; // 11.4
+  final messageTextController = TextEditingController(); // 14.17(a)
   final _auth = FirebaseAuth.instance; // 8.7(b)
   FirebaseUser loggedInUser; //8.7(d)
 
@@ -49,8 +51,8 @@ class _ChatScreenState extends State<ChatScreen> {
       print(message.data);
     }
   }
-
    */
+
   // 12.4 method listens to messages from FireStore (Firestore QuerySnapshot)
   void messagesStream() async {
     await for (var snapshot in _firestore.collection('messages').snapshots()) {
@@ -85,36 +87,7 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            // 13.1
-            StreamBuilder<QuerySnapshot>(
-              stream: _firestore.collection('messages').snapshots(), // 13.1(a)
-              // ignore: missing_return
-              builder: (context, snapshot) {
-                // 13.1(b) first check if snapshot has data!
-                if (!snapshot.hasData) {
-                  // 13.2 if snapshot has no data -> show spinner:
-                  return Center(
-                    child: CircularProgressIndicator(
-                      backgroundColor: Colors.lightBlueAccent,
-                    ),
-                  );
-                } else {
-                  // 13.1(c)
-                  final messages = snapshot.data.documents; // needs <QuerySnapshot> above to access .documents
-                  List<Text> messageWidgets = []; // List of Text Widgets to be created
-                  // 13.1 (d) use FOR_LOOP to create TextWidgets:
-                  for (var message in messages) {
-                    final messageText = message.data['text']; // different from snapshot.data!!!
-                    final messageSender = message.data['sender'];
-                    final messageWidget = Text('$messageText from $messageSender'); // create new widget
-                    messageWidgets.add(messageWidget); // add new MessageWidget to List of Widgets
-                  }
-                  return Column(children: messageWidgets); // 13.1(e) return column with list of text widgets
-                }
-              }, // builder: logic what StreamBuilder should do when rebuilding |
-              // builder snapshot is not the same as Firebase QuerySnapshot above!!, it's a Flutter AsyncSnapshot,
-              // but contains QuerySnapshot!
-            ),
+            MessagesStream(),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -122,6 +95,7 @@ class _ChatScreenState extends State<ChatScreen> {
                 children: <Widget>[
                   Expanded(
                     child: TextField(
+                      controller: messageTextController, // 14.17(b)
                       onChanged: (value) {
                         messageText = value; // 11.3
                       },
@@ -130,6 +104,8 @@ class _ChatScreenState extends State<ChatScreen> {
                   ),
                   FlatButton(
                     onPressed: () {
+                      // 14.17(c) clear TextField when pressed
+                      messageTextController.clear();
                       // 11.5 INSERT data into Firestore
                       _firestore.collection('messages').add({
                         'text': messageText,
@@ -146,6 +122,108 @@ class _ChatScreenState extends State<ChatScreen> {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+// 14.16(a)
+class MessagesStream extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // 13.1 & 14.16(b)
+    return StreamBuilder<QuerySnapshot>(
+      stream: _firestore.collection('messages').snapshots(), // 13.1(a)
+      // ignore: missing_return
+      builder: (context, snapshot) {
+        // 13.1(b) first check if snapshot has data!
+        if (!snapshot.hasData) {
+          // 13.2 if snapshot has no data -> show spinner:
+          return Center(
+            child: CircularProgressIndicator(
+              backgroundColor: Colors.lightBlueAccent,
+            ),
+          );
+        } else {
+          // 13.1(c)
+          final messages = snapshot.data.documents; // needs <QuerySnapshot> above to access .documents
+          List<MessageBubble> messageBubbles = []; // List of Text Widgets to be created & 14.6
+          // 13.1 (d) use FOR_LOOP to create TextWidgets:
+          for (var message in messages) {
+            final messageText = message.data['text']; // different from snapshot.data!!!
+            final messageSender = message.data['sender'];
+            // // 14.4 create new widget - initially of Type Text, now of custom Type MessageBubble
+            final messageBubble = MessageBubble(
+              sender: messageSender,
+              text: messageText,
+            );
+            messageBubbles.add(messageBubble); // add new MessageWidget to List of Widgets
+          }
+          // return Column(children: messageWidgets); // 13.1(e) return column with list of text widgets-
+          // OBSOLETE per 14.1
+          // 14.1
+          return Expanded(
+            child: ListView(
+              padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0), // 14.2
+              children: messageBubbles,
+            ),
+          );
+        }
+      }, // builder: logic what StreamBuilder should do when rebuilding |
+      // builder snapshot is not the same as Firebase QuerySnapshot above!!, it's a Flutter AsyncSnapshot,
+      // but contains QuerySnapshot!
+    );
+  }
+}
+
+// 14.3(a)
+class MessageBubble extends StatelessWidget {
+  // 14.3(c)
+  final String sender;
+  final String text;
+
+  MessageBubble({this.sender, this.text}); // 14.3(d)
+
+  @override
+  Widget build(BuildContext context) {
+    // 14.8
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      // 14.13
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end, // 14.15
+        children: <Widget>[
+          // 14.14
+          Text(
+            sender,
+            style: TextStyle(
+              fontSize: 12.0,
+              color: Colors.black54,
+            ),
+          ),
+          // 14.7(a)
+          Material(
+            // 14.12
+            borderRadius: BorderRadius.circular(30.0),
+            // 14.11
+            elevation: 5.0,
+            //14.7(b)
+            color: Colors.lightBlueAccent,
+            // 14.10
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              child: Text(
+                // 14.3(b)&(e)
+                text,
+                // 14.9
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15.0,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
